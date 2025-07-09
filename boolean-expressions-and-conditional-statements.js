ebee123767ae96a61672700161f479248cc60b2d
Viji Ramu <vijiramu@gmail.com>
2025-07-08 23:14:57 -0500

    Boolean

--- boolean-expressions-and-conditional-statements.js
+++ boolean-expressions-and-conditional-statements.js
@@ -20,9 +20,9 @@ Starter Code:
   - Run the following command in your terminal to install the readline-sync module:
     npm install readline-sync
 
-Paste the following code into your editor:
+Paste the following code into your editor:mo
 
-*/
+
 
 const readline = require('readline-sync');
 
@@ -42,7 +42,7 @@ if (choice === "mountains" && hasTorch) {
   console.log("You get lost and wander aimlessly.");
 }
 
-/* 
+
 
 Add Customization and expand the game:
   - Add more choices and scenarios.
@@ -50,3 +50,33 @@ Add Customization and expand the game:
   - Use nested conditionals and logical operators to create complex outcomes.
 
 */
+
+const readline = require('readline-sync');
+
+const hasTorch = true;
+const hasMap = false;
+const hasSword = true;
+const hasCompass = false;
+
+console.log("You see two paths: one leads to the mountains, the other to the village.");
+const choice = readline.question("Do you go to the 'mountains' or the 'village'?");
+
+console.log("Do you have the torch ?");
+
+if (choice === "mountains" && hasTorch  && hasSword || hasCompass) {
+  console.log("You safely navigate through the dark mountains. There could be wild creatures on your way. Make use of the torch");
+} 
+else if (choice === "mountains" && !hasTorch && hasSword && hasCompass) {
+  console.log("It's too dark to proceed. You decide to turn back or be brave to proceed with Sword for safety and compass for direction.");
+} 
+else if (choice === "village" || !hasMap) {
+  console.log("You find your way to the village.");
+}
+  else if (choice === "village" || !hasCompass && hasSword){
+    console.log("Have a safe travel to the village");
+} 
+else {
+  console.log("You get lost and wander aimlessly.");
+}
+
+/*if mountain and has torch, ask if he have sword and compass, be awa */
